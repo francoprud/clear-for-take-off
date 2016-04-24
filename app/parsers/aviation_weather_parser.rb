5098c9c05aa0e2d07d4437bae75823e6c586efa8
Franco Prudhomme <francoprud@gmail.com>
2016-04-24 07:06:36 -0300

    Adds small refactor

--- app/parsers/aviation_weather_parser.rb
+++ app/parsers/aviation_weather_parser.rb
@@ -3,13 +3,15 @@ class AviationWeatherParser
   include Api::V1::AirportsHelper
   attr_reader :params, :time, :parsed_response
 
+  BASE_URL = 'http://www.aviationweather.gov/adds/dataserver_current/httpparam?'
+
   def initialize(parameters)
     @params = parameters
     @time = date_in_seconds_from(parameters[:date], parameters[:hour])
   end
 
   def parse_information
-    url = 'http://www.aviationweather.gov/adds/dataserver_current/httpparam?'
+    url = BASE_URL
     url << 'dataSource=tafs'
     url << '&requestType=retrieve'
     url << '&format=xml'
@@ -17,7 +19,7 @@ class AviationWeatherParser
     url << '&mostRecent=true'
     url << '&timeType=issue'
     url << '&stationString='
-    url_for_origin = url +  (aviation_wheather_code_by @params['origin'])
+    url_for_origin = url + (aviation_wheather_code_by @params['origin'])
     url_for_destination = url + (aviation_wheather_code_by @params['destination'])
     @origin_parsed_response = ::HTTParty.get(url_for_origin).parsed_response
     @destination_parsed_response = ::HTTParty.get(url_for_destination).parsed_response
@@ -36,18 +38,17 @@ class AviationWeatherParser
       "origin_response" => {
         wind_speed: origin_forecast['wind_speed_kt'],
         visibility: origin_forecast['visibility_statute_mi'],
-        wind_bearing: origin_forecast['wind_dir_degrees'],
+        wind_bearing: origin_forecast['wind_dir_degrees']
       },
       "destination_response" => {
-          wind_speed: destination_forecast['wind_speed_kt'],
-          visibility: destination_forecast['visibility_statute_mi'],
-          wind_bearing: destination_forecast['wind_dir_degrees'],
-        }
+        wind_speed: destination_forecast['wind_speed_kt'],
+        visibility: destination_forecast['visibility_statute_mi'],
+        wind_bearing: destination_forecast['wind_dir_degrees']
       }
+    }
   end
 
-def utc_time
-  @time - 60*60*3
-end
-
+  def utc_time
+    @time - 60*60*3
+  end
 end
